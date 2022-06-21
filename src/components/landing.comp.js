1a8c2fe46a80dd341df0f2fd9e102a454ee72e97
Brandon <brandon.michael.burt@gmail.com>
2022-06-21 19:09:26 -0400

    Added some basic functionality, including an initial query to the All Day smart contract

---
+++ src/components/landing.comp.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react"
+import { query } from "@onflow/fcl"
+import { GET_ALL_SERIES } from '../scripts/get-all-series.script';
+
+const getAllDaySeries = async () => {
+    const res = await query({
+      cadence: GET_ALL_SERIES
+    });
+    return res;
+}
+
+  export function Landing() {
+    const [error, setError] = useState(null);
+    const [allDayData, setAllDayData] = useState();
+    useEffect(() => {
+        getAllDaySeries()
+        .then((d) => {
+          setAllDayData(d);
+        })
+        .catch(() => setError(true))
+    }, []);
+
+    if (error != null) {
+        return (
+            <h3>
+                <span>Error Fetching All Day Info: {error}</span>
+            </h3>
+        )
+    }
+
+    return (
+        <h2>
+            All Day
+        </h2>
+    )
+
+}
---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+import { config } from '@onflow/fcl';
+
+config({
+    "accessNode.api": "https://rest-mainnet.onflow.org",
+    "0xAllDay": "0xe4cf4bdc1751c65d",
+});
