a8a2ef1841ffbea14f60e14767d7ee7c42581060
Brandon <brandon.michael.burt@gmail.com>
2022-06-21 21:46:58 -0400

    Added a few more scripts

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,21 +1,72 @@
 import React, { useState, useEffect } from "react"
 import { query } from "@onflow/fcl"
 import { GET_ALL_SERIES } from '../scripts/get-all-series.script';
+import { GET_ALL_SETS } from '../scripts/get-all-sets.script';
+import { GET_ALL_EDITIONS } from '../scripts/get-all-editions.script';
+import { GET_TOTAL_SUPPLY } from "../scripts/get-total-supply.script";
 
-const getAllDaySeries = async () => {
+const getAllSeries = async () => {
     const res = await query({
       cadence: GET_ALL_SERIES
     });
     return res;
 }
 
+const getAllSets = async () => {
+    const res = await query({
+      cadence: GET_ALL_SETS
+    });
+    return res;
+}
+
+const getAllEditions = async () => {
+    const res = await query({
+      cadence: GET_ALL_EDITIONS
+    });
+    return res;
+}
+
+const getTotalSupply = async() => {
+    const res = await query({
+        cadence: GET_TOTAL_SUPPLY
+    });
+    return res;
+}
+
 export function Home() {
     const [error, setError] = useState(null);
-    const [allDayData, setAllDayData] = useState();
+    const [series, setSeries] = useState([]);
     useEffect(() => {
-        getAllDaySeries()
+        getAllSeries()
         .then((d) => {
-            setAllDayData(d);
+            setSeries(d);
+        })
+        .catch(() => setError(true))
+    }, []);
+
+    const [sets, setSets] = useState([]);
+    useEffect(() => {
+        getAllSets()
+        .then((d) => {
+            setSets(d);
+        })
+        .catch(() => setError(true))
+    }, []);
+
+    const [editions, setEditions] = useState([]);
+    useEffect(() => {
+        getAllEditions()
+        .then((d) => {
+            setEditions(d);
+        })
+        .catch(() => setError(true))
+    }, []);
+
+    const [totalSupply, setTotalSupply] = useState();
+    useEffect(() => {
+        getTotalSupply()
+        .then((d) => {
+            setTotalSupply(d);
         })
         .catch(() => setError(true))
     }, []);
@@ -29,9 +80,14 @@ export function Home() {
     }
 
     return (
-        <h2>
-            All Day
-        </h2>
+        <div>
+            <h2>
+                Moments in existence: {totalSupply}
+            </h2>
+            <div>
+                {series.map(x => <p>{x.name}</p>)}
+            </div>
+        </div>
     )
 
 }
---
+++ src/scripts/get-total-supply.script.js
@@ -0,0 +1,7 @@
+export const GET_TOTAL_SUPPLY = `
+    import AllDay from 0xAllDay
+
+    pub fun main(): UInt64 {    
+        return AllDay.totalSupply
+    }
+`;
