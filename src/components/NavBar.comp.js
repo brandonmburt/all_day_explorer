307d5ade77639a29e65edb9f8edd4e3d56f5e0c0
Brandon <brandon.michael.burt@gmail.com>
2022-06-26 23:04:03 -0400

    Created navigation bar component

---
+++ src/components/NavBar.comp.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useHistory } from "react-router-dom"
+
+export default function Navbar() {
+  const history = useHistory()
+
+  const NavItem = ({ route }) => (
+    <div className="navbar__item">
+      <div className="btn" onClick={() => history.push(route.path)}>
+        {route.name}
+      </div>
+    </div>
+  )
+
+  return (
+    <div className="navbar">
+      {/* {NAV_ROUTES.map(item => <NavItem route={item} key={item.path} />)} */}
+    </div>
+  )
+}
