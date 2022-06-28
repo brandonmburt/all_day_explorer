91e11e962173a69b919d0204adf34013c434ad69
Brandon <brandon.michael.burt@gmail.com>
2022-06-28 10:21:52 -0400

    Removed unnecessary logic from Home page

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,23 +1,11 @@
 import React, { useState } from "react"
-import { useSeries } from '../providers/SeriesProvider.comp';
-import { useSets } from '../providers/SetsProvider.comp';
-import { useEditions } from "../providers/EditionsProvider.comp";
 import useTotalSupply from '../hooks/use-total-supply.hook';
-import { getSetsWithinSeries } from '../utils/nav.utils';
-import { SeriesNav } from "../components/SeriesNav.comp";
 
 export function Home() {
-    const { series } = useSeries();
-    const { sets } = useSets();
-    const { editions } = useEditions();
+
     const [ error, setError ] = useState(null);
 
     const [totalSupply] = useTotalSupply();
-
-    let seriesSets = getSetsWithinSeries(series, sets, editions);
-    if (seriesSets !== null) {
-        console.log(seriesSets);
-    }
 
     if (error != null) {
         return (
@@ -32,14 +20,6 @@ export function Home() {
             <h2>
                 Moments in existence: {totalSupply}
             </h2>
-            <div>
-                {seriesSets !== null &&
-                    Array.from(seriesSets).map(([key, val]) => {
-                        const s = series.get(key);
-                        return <SeriesNav key={key} id={s.id} name={s.name} active={s.active} sets={val} />
-                    })
-                }
-            </div>
         </div>
     )
 
