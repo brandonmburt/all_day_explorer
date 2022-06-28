05843b717e632a955c6cfb5db2ca22dc4bfe972a
Brandon <brandon.michael.burt@gmail.com>
2022-06-28 19:49:41 -0400

    Added page to display all plays using react-data-table-component

---
+++ src/pages/Plays.page.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { usePlays } from "../providers/PlaysProvider.comp";
+import DataTable from 'react-data-table-component';
+import { Container } from "react-bootstrap"
+
+const columns = [
+    {
+        name: 'ID',
+        selector: row => +row.id,
+        sortable: true,
+    },
+    {
+        name: 'Player',
+        selector: row => [row.metadata.playerFirstName, row.metadata.playerLastName].join(' '),
+        sortable: true,
+    },
+    {
+        name: 'Team',
+        selector: row => row.metadata.teamName,
+        sortable: true,
+    },
+    {
+        name: 'Position',
+        selector: row => row.metadata.playerPosition,
+        sortable: true,
+    },
+    {
+        name: 'Play Type',
+        selector: row => row.classification === "PLAYER_GAME" ? row.metadata.playType : "Team Melt",
+        sortable: true,
+    },
+    {
+        name: 'Game Date',
+        selector: row => row.metadata.gameDate,
+        sortable: true,
+    }
+];
+
+export function Plays() {
+
+    const { plays } = usePlays();
+
+    const playArr = [];
+    plays.forEach(play => playArr.push(play));
+
+    return (
+        <Container>
+            <DataTable
+                title="Plays"
+                columns={columns}
+                data={playArr}
+                defaultSortFieldId={1}
+                pagination
+            />
+        </Container>
+    )
+
+}
