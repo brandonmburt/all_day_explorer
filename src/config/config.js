14a56ec7f995c575cb39e695202943ae81be3923
Brandon <brandon.michael.burt@gmail.com>
2022-07-06 22:02:51 -0400

    Added config for NFT standard contract

--- src/config/config.js
+++ src/config/config.js
@@ -3,4 +3,5 @@ import { config } from '@onflow/fcl';
 config({
     "accessNode.api": "https://rest-mainnet.onflow.org",
     "0xAllDay": "0xe4cf4bdc1751c65d",
+    "0xNFT": "0x1d7e57aa55817448",
 });
