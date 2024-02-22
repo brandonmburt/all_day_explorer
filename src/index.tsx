012dab89026242a0cddbad3fc9824bf7c9b2198e
Brandon <brandon.michael.burt@gmail.com>
2024-02-22 16:09:05 -0500

    Added SASS. Converted CSS to SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles.scss';
 // import reportWebVitals from './reportWebVitals';
 import './config/config';
 import Providers from './providers/Providers.comp';
