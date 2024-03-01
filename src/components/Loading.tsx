da353b08edaf3adce1eaf38926e5e2d4d2d65a33
Brandon <brandon.michael.burt@gmail.com>
2024-03-01 00:30:45 -0500

    Updated style of loading indicator

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,14 +3,9 @@ import ReactLoading from "react-loading";
 export function Loading() {
 
     return (
-        <>
-            <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <ReactLoading type="spin" color="gray" height="130px" width="130px" delay={100} />
-            </div>
-            <div style={{ marginTop: '20px', width: '100%', textAlign: 'center' }}>
-                <h5>Fetching Data</h5>
-            </div>
-        </>
+        <div className="loading-container">
+            <ReactLoading type="spin" color="gray" height="100px" width="130px" delay={100} />
+        </div>
     )
 
 }
